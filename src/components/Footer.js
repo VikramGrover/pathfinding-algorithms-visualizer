26e90b63ca265c7aad8d913fbfaf53f1ed2fe280
Vikram Grover <vik.grover71@gmail.com>
2021-08-15 00:41:03 -0400

    Fix vertical alignment issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='footer'>
-            Built by <a href='https://vikgrover.me' target='_blank'>Vikram Grover</a> | <a href='https://github.com/VikramGrover/pathfinding-algorithms-visualizer' target='_blank'>GitHub Repo</a>
+            Built by <a href='https://vikgrover.me' target='_blank'>Vikram Grover</a>
         </div>
     )
 }
